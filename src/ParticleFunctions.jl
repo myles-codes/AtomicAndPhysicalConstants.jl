5bb64c1d1aae79a1f307caf6a645fb55735d0c89
Alex Coxe <rot4te@gmail.com>
2023-11-20 14:28:32 -0500

    updated functions to allow modifying the charge of atoms in the name of the atom, and reordered the ParticleSpecies.jl dictionaries

--- src/ParticleFunctions.jl
+++ src/ParticleFunctions.jl
@@ -1,38 +1,40 @@
 # AtomicAndPhysicalConstants.jl/ParticleFunctions.jl
+m = "black"
+l = findfirst('j', m)
+c = findfirst('c', m)
+l>c
 
-
-
+tryparse(Int, "++")
 # ------------------------------------------------------------------------------------------------------------
 
 
 """
-function set_species:
-* arguments: (species_name::AbstractString, [isotope::Integer], [charge::Integer])
-* returns ref_particle::AbstractSpecies
-\n
-description:
-> routine to set an object for use in simulations and computations given the name of the particle <
-\n
-inputs:
-  - species_name::AbstractString        -- name of particle species,
-                                         if blank function breaks
-  - [charge::Integer]                   -- set the charge of an atomic particle,
-                                         the default behavior uses a neutral atom
-  - [isotope::Integer]                  -- use a particular atomic isotope,
-                                         the default behavior is to use the average atomic mass
+    function `set_species`(species_name::AbstractString, [isotope]::Integer=-1, [charge]::Integer=1)
 
-output:
-  -ref_particle::AbstractSpecies       -- immutable particle object for use in 
-                                         simulations 
-\n
-notes:
-> set_species searches the subatomic and atomic particle dictionaries for a particle 
-> matching positional arg species_name = Dict(key).species_name (with key = species_name) 
+### Description:
+> routine to set an object for use in simulations and computations given the name of the particle <
+
+### Input:
+- `species_name`   -- name of particle species,if blank function breaks
+- `charge`         -- (optional, default: '0')  charge of an atomic particle,
+- `isotope`        -- (optional, default: '-1') selected atomic isotope, 
+                            default uses the average
+
+### Output:
+- `ref_particle`      -- immutable particle object for use in 
+                         simulations 
+
+### Notes:
+> `set_species` searches the subatomic and atomic particle dictionaries for a particle 
+> matching positional arg `species_name` = `Dict(key).species_name` (with key = `species_name`) 
 > and if found constructs an AbstractSpecies object containing the data associated to
 > species_name for use in computation/simulation:
-> optional charge and isotope keyword args come into play if and only if species_name
+> optional charge and isotope keyword args come into play if and only if `species_name`
 > refers to an atomic particle; charge allows a non-neutral atom, and isotope allows 
-> selection of any available isotope mass <
+> selection of any available isotope mass 
+> if `species_name` is an atomic sybmol with a charge modifier, _eg_ Li+2 or Na-1, the function
+> treats the modifier as the net charge, which is to say
+> `set_species`("Pb", charge=5) = `set_species`("Pb+5")
 """ set_species
 
 function set_species(species_name::AbstractString, charge::Int=0, isotope::Int=-1)
@@ -42,14 +44,31 @@ function set_species(species_name::AbstractString, charge::Int=0, isotope::Int=-
     # now set the reference particle
     ref_particle = SubatomicSpecies(ref.species_name, ref.charge, ref.mass, 
                                     Dict("anomalous_moment" => ref.anomalous_moment, "spin" => ref.spin))
-  #= then check if the specified particle is one of the known atoms: 
-  if no isotope is given, use the common average mass =#
-  elseif haskey(Atomic_Particles, species_name)
-    ref = Atomic_Particles[species_name] # pull in the info so we don't make repeated dictionary calls
-    # set the reference particle
+  #  then check if the specified particle is one of the known atoms: 
+  # if no isotope is given, use the common average mass 
+  elseif haskey(Atomic_Particles, species_name) == true
+    ref = Atomic_Particles[species_name]
+    ref_particle = AtomicSpecies(ref.species_name, ref.Z, charge, ref.mass[isotope])
+
+  # check if the first two chars of species_name make up an atomic symbol
+  elseif haskey(Atomic_Particles, species_name[1]) == true || haskey(Atomic_Particles, species_name[1:2]) == true
+    p = findfirst('+', species_name) # see if the name modifies the charge up
+    m = findfirst('-', species_name) # or down
+    if typeof(m) == Nothing && typeof(p) == Int # if positive net charge
+      ref = Atomic_Particles[species_name[1:p-1]] # grab the element data
+      charge = tryparse(Int, species_name[p:end]) # and set the final charge
+    elseif typeof(p) == Nothing && typeof(m) == Int # if negative net charge
+      ref = Atomic_Particles[species_name[1:m-1]] # grab the element data
+      charge = tryparse(Int, species_name[m:end]) # and set the final charge
+    end
+    # build a final static particle
     ref_particle = AtomicSpecies(ref.species_name, ref.Z, charge, ref.mass[isotope])
   end
-  return ref_particle # return the static reference particle
+  if abs(charge) > 127
+    error(f"|charge| = {abs(charge)}, which is too big")
+  else
+    return ref_particle # return the static reference particle
+  end
 end; export set_species
 
 
@@ -57,32 +76,30 @@ end; export set_species
 
 
 """
-function charge_per_mass:
-* arguments: (species_name::AbstractString, [isotope::Integer], [charge::Integer])
-* returns: charge_per_mass::Float64
-\n
-description: 
+    function `charge_per_mass`(`species_name`::AbstractString, [isotope]::Integer, [charge]::Integer)
+
+### Description: 
 > Routine takes in the name of a particle and optionally a charge and isotope number,
 > to return charge/mass <
-\n
-inputs:
-  - species_name::AbstractString        -- name of particle species,
-                                         if blank function breaks
-  - [charge::Integer]                   -- set the charge of an atomic particle,
-                                         the default behavior uses a neutral atom
-  - [isotope::Integer]                  -- use a particular atomic isotope,
-                                         the default behavior is to use the average atomic mass
-\n
-output:
-  - charge_per_mass::Float64            -- ratio of integer charge to mass of given particle
-\n
-notes:
+
+### Inputs:
+- `species_name`       -- name of particle species, if blank function breaks
+- `charge`             -- (optional, default: '0')  charge of an atomic particle,
+- `isotope`            -- (optional, default: '-1') selected atomic isotope, 
+                            default uses the average
+### Output:
+- `charge_per_mass`    -- ratio of integer charge to mass of given particle
+
+### Notes:
 > function searches the known dictionaries of subatomic particles for a particle matching 
-> species_name = Dict(key).species_name (with key = species_name) and if found returns 
+> `species_name` = `Dict(key).species_name` (with key = `species_name`) and if found returns 
 > Dict(key).charge/Dict(key).mass:
-> optional 'charge' and 'isotope' arguments only have any effect if species_name refers to an 
-> atomic particle: in this case the atom is taken to have charge = charge::Int, and the atomic 
-> mass is set by the isotope <
+> optional `charge` and `isotope` arguments only have any effect if `species_name` refers to an 
+> atomic particle: in this case the atom is taken to have charge = `charge`::Int, and the atomic 
+> mass is set by the isotope 
+> if `species_name` is an atomic sybmol with a charge modifier, _eg_ Li+2 or Na-1, the function
+> treats the modifier as the net charge, which is to say
+> `charge_per_mass`("He", charge=2) = `charge_per_mass`("He+2")<
 """ charge_per_mass
 
 function charge_per_mass(species_name::AbstractString, charge::Int = 0, isotope::Int = -1)
@@ -93,6 +110,16 @@ function charge_per_mass(species_name::AbstractString, charge::Int = 0, isotope:
   # check if atomic nucleus
   elseif haskey(Atomic_Particles, species_name) == true
     return charge/Atomic_Particles[species_name].mass[isotope]
+  elseif haskey(Atomic_Particles, species_name[1]) == true || haskey(Atomic_Particles, species_name[1:2]) == true
+    p = findfirst('+', species_name) # see if the name modifies the charge up
+    m = findfirst('-', species_name) # or down
+    if typeof(m) == Nothing && typeof(p) == Int # if positive net charge
+      charge = tryparse(Int, species_name[p:end]) # set the final charge
+      return charge/Atomic_Particles[species_name[1:p-1]].mass[isotope]
+    elseif typeof(p) == Nothing && typeof(m) == Int # if negative net charge
+      charge = tryparse(Int, species_name[m:end]) # and set the final charge
+      return charge/Atomic_Particles[species_name[1:m-1]].mass[isotope]
+    end
   end
 end; export charge_per_mass
 
