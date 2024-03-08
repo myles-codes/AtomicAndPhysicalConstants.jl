c5f47eecf1351a26fc47fba3dd7a7046164e0df1
lixing <lilixing125@gmail.com>
2024-03-07 22:35:11 -0500

    added the program for extracting pion mass data from particle data group

--- src/GetPionData.jl
+++ src/GetPionData.jl
@@ -6,7 +6,7 @@ Pkg.add("JSON")
 using HTTP
 using JSON
 
-# extracting informations from the json file
+# extracting informations from the S009 json file
 url = "https://pdgapi.lbl.gov/summaries/S009"
 response = HTTP.get(url)
 info = JSON.parse(String(response.body))
@@ -14,10 +14,22 @@ info = JSON.parse(String(response.body))
 # the dictionary that maps the name of the particle to the mass unit is MeV
 Mass = Dict{String, Float64}()
 
+# the information of the mass is only at 1, 2
 for i in 1:2
     Mass[info["summaries"]["properties"][i]["description"]] = info["summaries"]["properties"][i]["pdg_values"][1]["value"]
 end
 
+# extracting informations from the S008 json file
+url = "https://pdgapi.lbl.gov/summaries/S008"
+response = HTTP.get(url)
+info= JSON.parse(String(response.body))
+
+# the information of the mass is only at 1, 3
+for i in 1:2:3
+    Mass[info["summaries"]["properties"][i]["description"]] = info["summaries"]["properties"][i]["pdg_values"][1]["value"]
+end
+
+#print the dictionary
 println(Mass)
 
 
