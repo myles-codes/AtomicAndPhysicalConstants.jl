0d5b965a1830bc7b6a110b9053bedaf98e100ef2
lixing <lilixing125@gmail.com>
2024-03-13 17:34:41 -0400

    change function  name to 'pdg_pion_masses'

--- src/UpdatePionMass.jl
+++ src/UpdatePionMass.jl
@@ -6,16 +6,17 @@ Pkg.add("JSON")
 using HTTP
 using JSON
 
-# The function getMasses gets the masses of pion from Particle Data Group
-# It returns the mass of pion 0, pion +, pion - in order as a tuple, the unit is MeV
-
-function getMasses()
+"""
+pdg_pion_masses() gets the masses of pion from Particle Data Group.
+It returns the mass of pion 0, pion +, pion - in order as a tuple, the unit is eV/c^2
+"""
+function pdg_pion_masses()
     # extracting informations from the S009 json file
     url = "https://pdgapi.lbl.gov/summaries/S009"
     response = HTTP.get(url)
     info = JSON.parse(String(response.body))
 
-    # the dictionary that maps the name of the particle to the mass unit is eV
+    # the dictionary that maps the name of the particle to the mass unit is eV/c^2
     mass = Dict{String, Float64}()
 
     # the information of the mass is only at 1
@@ -37,5 +38,3 @@ function getMasses()
     return [mass["pi0 MASS"],mass["pi+- MASS"],mass["pi+- MASS"]] 
 
 end
-
-getMasses()
