eba575a7bb4b085607e02f8ccd912641c4d4f2bc
lixing <lilixing125@gmail.com>
2024-03-13 12:27:19 -0400

    changed pion mass unit to eV

--- src/UpdatePionMass.jl
+++ src/UpdatePionMass.jl
@@ -15,12 +15,12 @@ function getMasses()
     response = HTTP.get(url)
     info = JSON.parse(String(response.body))
 
-    # the dictionary that maps the name of the particle to the mass unit is MeV
+    # the dictionary that maps the name of the particle to the mass unit is eV
     mass = Dict{String, Float64}()
 
     # the information of the mass is only at 1
     for i in [1]
-        mass[info["summaries"]["properties"][i]["description"]] = info["summaries"]["properties"][i]["pdg_values"][1]["value"]
+        mass[info["summaries"]["properties"][i]["description"]] = info["summaries"]["properties"][i]["pdg_values"][1]["value"]*10^6
     end
 
     # extracting informations from the S008 json file
@@ -30,10 +30,12 @@ function getMasses()
 
     # the information of the mass is only at 1
     for i in [1]
-        mass[info["summaries"]["properties"][i]["description"]] = info["summaries"]["properties"][i]["pdg_values"][1]["value"]
+        mass[info["summaries"]["properties"][i]["description"]] = info["summaries"]["properties"][i]["pdg_values"][1]["value"]*10^6
     end
 
     # return the mass of pion 0, pion +, pion -
     return [mass["pi0 MASS"],mass["pi+- MASS"],mass["pi+- MASS"]] 
 
 end
+
+getMasses()
