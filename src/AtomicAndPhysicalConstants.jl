2ef39f57ca0952645686b1be8f8f30ff00b92ef1
Alex Coxe <rot4te@gmail.com>
2024-04-09 12:13:40 -0400

    added Global.jl to hold the non-consts

--- src/AtomicAndPhysicalConstants.jl
+++ src/AtomicAndPhysicalConstants.jl
@@ -3,6 +3,7 @@ module AtomicAndPhysicalConstants
 using PyFormattedStrings
 
 include("PhysicalConstants.jl")
+include("Global.jl")
 include("ParticleTypes.jl")
 include("AtomicIsotopes.jl")
 include("SubatomicSpecies.jl")
