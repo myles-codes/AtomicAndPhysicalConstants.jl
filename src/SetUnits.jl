3fe3bfe65d54945277f5b578989791b671865f45
lixing <lilixing125@gmail.com>
2024-04-09 23:02:59 -0400

    added time and speed

--- src/SetUnits.jl
+++ src/SetUnits.jl
@@ -181,7 +181,7 @@ MASS = (amu = Mass("amu",1.0),
     Length<:Unit
 
     ### Description:
-    > immutable struct to be used for storing length units<
+    > immutable struct for storing length units<
     > the basis for conversion is meter<
 
     ### Fields:
@@ -211,6 +211,70 @@ LENGTH = (m = Length("m",1.0),
     A = Length("Å",10^10))
 
 """
+    Time<:Unit
+
+    ### Description:
+    > immutable struct to be used for storing time units<
+    > the basis for conversion is second<
+
+    ### Fields:
+	- `name`                        -- type:AbstractString, name of the unit
+	- `conversion`                  -- type:FLoat, 'conversion' unit = 1 second
+
+""" Time
+
+struct Time<:Unit
+    name::AbstractString
+    conversion::Float64
+end
+
+"""
+    TIME
+    ### Description:
+    > a constant name tuple for storing all the units for time<
+    > units includes: s, minute, hour<
+
+    ### Example:
+    TIME.s is the unit 'second', it refers the the struct Length("s",1.0)
+
+""" TIME
+
+TIME = (s = Time("s",1.0),
+    minute = Time("minute",1/60),
+    hour = Time("hour",1/3600),)
+
+"""
+    Speed<:Unit
+
+    ### Description:
+    > immutable struct to be used for storing speed units<
+    > the basis for conversion is meter per second<
+
+    ### Fields:
+	- `name`                        -- type:AbstractString, name of the unit
+	- `conversion`                  -- type:FLoat, 'conversion' unit = 1 meter per second
+
+""" Speed
+
+struct Speed<:Unit
+    name::AbstractString
+    conversion::Float64
+end
+
+# Define devision operation that makes Speed from Length / Time 
+Base.:/(a::Length, b::Time) = Speed(a.name*"/"*b.name,a.conversion/b.conversion)
+
+"""
+    Speed
+    ### Description:
+    > a constant name tuple for storing all the units for speed<
+    > units includes: c, m/s<
+
+""" SPEED
+
+SPEED = (c = Speed("c",1/__b_c_light), m_per_s = LENGTH.m/TIME.s)
+
+"""
     UnitSystem
 
     ### Description:
@@ -219,18 +283,19 @@ LENGTH = (m = Length("m",1.0),
     ### Fields:
 	- `mass`                    -- type:Mass, stores the unit for mass
 	- `length`                  -- type:Length, stores the unit for length
+    - `time`                    -- type:Time, stores the unit for time
 
 """ UnitSystem
 
 struct UnitSystem
     mass::Mass
     length::Length
+    time::Time
 end
 
-PARTICLE_PHYSICS = UnitSystem(MASS.amu,LENGTH.m)
-MKS = UnitSystem(MASS.kg,LENGTH.m)
-CGS = UnitSystem(MASS.g,LENGTH.cm)
-
+PARTICLE_PHYSICS = UnitSystem(MASS.amu,LENGTH.m,TIME.s)
+MKS = UnitSystem(MASS.kg,LENGTH.m,TIME.s)
+CGS = UnitSystem(MASS.g,LENGTH.cm,TIME.s)
 
 
 """
@@ -239,6 +304,11 @@ CGS = UnitSystem(MASS.g,LENGTH.cm)
     ### Description:
     > set global non constants to the value in units specified<
     > users can specify the unit system and modify units in the system by keyword parameters<
+    
+    ### default units
+    mass: amu
+    length: m
+    time: s
 
     ### positional parameters:
     - 'unitsystem'                  -- type:UnitSystem, specify the unit system, default to PARTICLE_PHYSICS
@@ -246,17 +316,29 @@ CGS = UnitSystem(MASS.g,LENGTH.cm)
     ### keyword parameters:
 	- `mass`                        -- type:Mass, unit for mass, default to the mass unit in 'unitsystem'
 	- `length`                      -- type:Length, unit for length, default to the length unit in 'unitsystem'
+    - `time`                        -- type:Time, unit for time, default to the time unit in 'unitsystem'
+    - `speed`                       -- type:Speed, unit for speed, default to the length unit in 'unitsystem'/time unit in 'unitsystem'. speed is also default to 'length'/'time' unit unless specified. 
 
 """ setunits
 
-function setunits(unitsystem::UnitSystem=PARTICLE_PHYSICS; mass::Mass=unitsystem.mass, length::Length=unitsystem.length)
+function setunits(unitsystem::UnitSystem=PARTICLE_PHYSICS; 
+    mass::Mass = unitsystem.mass, 
+    length::Length = unitsystem.length,
+    time::Time = unitsystem.time,
+    speed::Speed = length/time
+    )
     
-    # convert all the mass variables
-    global m_electron = mass.conversion * __b_m_electron / __b_eV_per_amu     # Electron Mass 
-    global m_proton = mass.conversion * __b_m_proton / __b_eV_per_amu        # Proton Mass 
-    global m_neutron = mass.conversion * __b_m_neutron / __b_eV_per_amu       # Neutron Mass 
-    global m_muon = mass.conversion * __b_m_muon  / __b_eV_per_amu          # Muon Mass 
-    global m_helion = mass.conversion * __b_m_helion / __b_eV_per_amu        # Helion Mass He3 nucleus 
-    global m_deuteron = mass.conversion * __b_m_deuteron / __b_eV_per_amu      # Deuteron Mass 
+    # convert all the variables with dimension mass
+    global m_electron = mass.conversion * __b_m_electron / __b_eV_per_amu       # Electron Mass 
+    global m_proton = mass.conversion * __b_m_proton / __b_eV_per_amu           # Proton Mass 
+    global m_neutron = mass.conversion * __b_m_neutron / __b_eV_per_amu         # Neutron Mass 
+    global m_muon = mass.conversion * __b_m_muon  / __b_eV_per_amu              # Muon Mass 
+    global m_helion = mass.conversion * __b_m_helion / __b_eV_per_amu           # Helion Mass He3 nucleus 
+    global m_deuteron = mass.conversion * __b_m_deuteron / __b_eV_per_amu       # Deuteron Mass 
 
+    # convert all the variables with dimension length
+    global r_e = length.conversion * __b_r_e                                    # classical electron radius
+
+    # convert all the variables with dimension speed
+    global c_light = speed.conversion * __b_c_light                                    # classical electron radius
 end
