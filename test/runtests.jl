26fd99febbcee503ac5c8c205ee4c5d661b1ffc5
lixing <lilixing125@gmail.com>
2024-05-03 13:32:01 -0400

    test the unit test again

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,8 @@ using Test
 
 @testset "AtomicAndPhysicalConstants.jl" begin
     # Write your tests here.
+    @test !@isdefined c_light
+    setunits()
+    @test c_light == 2.99792458e8
+    @test e_charge == 1
 end
