0738254bb3837cffb3bdc01b65eb090b953e9144
Alex Coxe <rot4te@gmail.com>
2024-05-03 09:24:04 -0400

    added base specifiers to subatomic base particle defs and multiplied spins by hbar

--- src/SubatomicSpecies.jl
+++ src/SubatomicSpecies.jl
@@ -24,21 +24,21 @@ Subatomic_Particles["some-particle"] = SubatomicSpecies("some-particle", ...)
 """ Subatomic_Particles
 
 Subatomic_Particles = Dict{AbstractString, SubatomicSpecies}(
-  "pion0"             => SubatomicSpecies("pion0", 0, m_pion_0, 0.0, 0.0),
+  "pion0"             => SubatomicSpecies("pion0", 0, __b_m_pion_0, 0.0, 0.0),
   # const ref_species       = SubatomicSpecies("Ref_Particle", "Garbage!", 0, 0.0, 0.0, NaN)
-  "neutron"           => SubatomicSpecies("neutron", 0, m_neutron, anom_mag_moment_neutron, 0.5),
-  "deuteron"          => SubatomicSpecies("deuteron", 1, m_deuteron, anom_mag_moment_deuteron, 1.0),
-  "pion+"             => SubatomicSpecies("pion+", 1, m_pion_charged, 0.0, 0.0),
-  "antimuon"          => SubatomicSpecies("anti-muon", 1, m_muon, anom_mag_moment_muon, 0.5),
-  "proton"            => SubatomicSpecies("proton", 1, m_proton, anom_mag_moment_proton, 0.5),
-  "positron"          => SubatomicSpecies("positron", 1, m_electron, anom_mag_moment_electron, 0.5),
+  "neutron"           => SubatomicSpecies("neutron", 0, __b_m_neutron, __b_anom_mag_moment_neutron, 0.5*__b_h_bar_planck),
+  "deuteron"          => SubatomicSpecies("deuteron", 1, __b_m_deuteron, __b_anom_mag_moment_deuteron, 1.0*__b_h_bar_planck),
+  "pion+"             => SubatomicSpecies("pion+", 1, __b_m_pion_charged, 0.0, 0.0),
+  "antimuon"          => SubatomicSpecies("anti-muon", 1, __b_m_muon, __b_anom_mag_moment_muon, 0.5*__b_h_bar_planck),
+  "proton"            => SubatomicSpecies("proton", 1, __b_m_proton, __b_anom_mag_moment_proton, 0.5*__b_h_bar_planck),
+  "positron"          => SubatomicSpecies("positron", 1, __b_m_electron, __b_anom_mag_moment_electron, 0.5*__b_h_bar_planck),
   "photon"            => SubatomicSpecies("photon", 0, 0.0, 0.0, 0.0),
-  "electron"          => SubatomicSpecies("electron", -1, m_electron, anom_mag_moment_electron, 0.5),
-  "antiproton"        => SubatomicSpecies("anti-proton", -1, m_proton, anom_mag_moment_proton, 0.5),
-  "muon"              => SubatomicSpecies("muon", -1, m_muon, anom_mag_moment_muon, 0.5),
-  "pion-"             => SubatomicSpecies("pion-", -1, m_pion_charged, 0.0, 0.0),
-  "anti_deuteron"     => SubatomicSpecies("anti-deuteron", -1, m_deuteron, anom_mag_moment_deuteron, 1.0),
-  "anti_neutron"      => SubatomicSpecies("anti-neutron", 0, m_neutron, anom_mag_moment_neutron, 0.5)
+  "electron"          => SubatomicSpecies("electron", -1, __b_m_electron, __b_anom_mag_moment_electron, 0.5*__b_h_bar_planck),
+  "antiproton"        => SubatomicSpecies("anti-proton", -1, __b_m_proton, __b_anom_mag_moment_proton, 0.5*__b_h_bar_planck),
+  "muon"              => SubatomicSpecies("muon", -1, __b_m_muon, __b_anom_mag_moment_muon, 0.5*__b_h_bar_planck),
+  "pion-"             => SubatomicSpecies("pion-", -1, __b_m_pion_charged, 0.0, 0.0),
+  "anti_deuteron"     => SubatomicSpecies("anti-deuteron", -1, __b_m_deuteron, __b_anom_mag_moment_deuteron, 1.0*__b_h_bar_planck),
+  "anti_neutron"      => SubatomicSpecies("anti-neutron", 0, __b_m_neutron, __b_anom_mag_moment_neutron, 0.5*__b_h_bar_planck)
   # const anti_ref_species  = SubatomicSpecies("Anti_Ref_Particle", "Garbage!", 0, 0.0, 0.0, NaN)
 ); export Subatomic_Particles
 
