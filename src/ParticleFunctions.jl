2244f32ffda5407e2bd266493a73bec3edba826d
Alex Coxe <rot4te@gmail.com>
2024-06-03 17:31:21 -0400

    added atomicnumber function, working on Particle arguments with regex

--- src/ParticleFunctions.jl
+++ src/ParticleFunctions.jl
@@ -23,43 +23,49 @@ the library.
 Particle
 
 function Particle(name::String, charge=0, iso=-1)
-    if haskey(Atomic_Particles, name) # is the particle in the Atomic_Particles dictionary?
-        if iso ∉ keys(Atomic_Particles[name].mass) # error handling if the isotope isn't available
-            println("The isotope you specified is not available.")
-            println("Isotopes are specified by the atomic symbol and integer mass number.")
-            return
-        end
-        mass = begin
-            nmass = Atomic_Particles[name].mass[iso] # mass of the neutrally charged isotope in amu
-            nmass * (kg_per_amu / kg_per_eV) + m_electron * (Atomic_Particles[name].Z - charge) # put it in eV/c^2 and remove the electrons
-        end
-        if iso == -1 # if it's the average, make an educated guess at the spin
-            partonum = round(nmass)
-            spin = 0.5 * partonum
-        else # otherwise, use the sum of proton and neutron spins
-            spin = 0.5 * iso
-        end
-        return Particle(name, charge, mass, spin, 0) # return the object to track
 
-    elseif haskey(Subatomic_Particles, name) # is the particle in the Subatomic_Particles dictionary?
-        # write the particle out directly
-        return Particle(name, Subatomic_Particles[name].charge,
-            Subatomic_Particles[name].mass,
-            Subatomic_Particles[name].spin,
-            Subatomic_Particles[name].anomalous_moment)
+	# define regex for the name String
+	rgm = r"#[0-9]|#[0-9][0-9]|#[0-9][0-9][0-9]"
+	rgcp = r"+|++|+++|+[0-9]|+[0-9][0-9]|+[0-9][0-9][0-9]"
+	rgcm = r"-|--|---|-[0-9]|-[0-9][0-9]|-[0-9][0-9][0-9]"
 
-    else # handle the case where the given name is garbage
-        println("The specified particle name does not exist in this library.")
-        println("Available subatomic particles are: ")
-        for p in keys(Subatomic_Particles)
-            println(p)
-        end
-        println("Available atomic elements are")
-        for p in keys(Atomic_Particles)
-            println(p)
-        end
-        return
-    end
+	if haskey(Atomic_Particles, name) # is the particle in the Atomic_Particles dictionary?
+		if iso ∉ keys(Atomic_Particles[name].mass) # error handling if the isotope isn't available
+			println("The isotope you specified is not available.")
+			println("Isotopes are specified by the atomic symbol and integer mass number.")
+			return
+		end
+		mass = begin
+			nmass = Atomic_Particles[name].mass[iso] # mass of the neutrally charged isotope in amu
+			nmass * (kg_per_amu / kg_per_eV) + m_electron * (Atomic_Particles[name].Z - charge) # put it in eV/c^2 and remove the electrons
+			end
+		if iso == -1 # if it's the average, make an educated guess at the spin
+			partonum = round(nmass)
+			spin = 0.5 * partonum
+		else # otherwise, use the sum of proton and neutron spins
+			spin = 0.5 * iso
+		end
+			return Particle(name, charge, mass, spin, 0) # return the object to track
+
+	elseif haskey(Subatomic_Particles, name) # is the particle in the Subatomic_Particles dictionary?
+		# write the particle out directly
+		return Particle(name, Subatomic_Particles[name].charge,
+			Subatomic_Particles[name].mass,
+			Subatomic_Particles[name].spin,
+			Subatomic_Particles[name].anomalous_moment)
+
+	else # handle the case where the given name is garbage
+		println("The specified particle name does not exist in this library.")
+		println("Available subatomic particles are: ")
+		for p in keys(Subatomic_Particles)
+			println(p)
+		end
+		println("Available atomic elements are")
+		for p in keys(Atomic_Particles)
+			println(p)
+		end
+		return
+	end
 
 end
 
@@ -91,4 +97,14 @@ function charge_per_mass(particle::Particle)
 end; export charge_per_mass
 
 
+function atomicnumber(particle::Particle)
 
+	if haskey(Atomic_Particles, particle.name)
+		return Atomic_Particles[name].Z
+
+	else
+		print(f"{particle.name} is not an atom, and thus no atomic number.")
+		return
+	end
+
+end
