474a0860a589e38e9b31f3c7be7457136ed12eba
lixing <lilixing125@gmail.com>
2024-09-23 20:52:18 -0400

    added all constants, reimplemented massof, chargof

--- src/AtomicAndPhysicalConstants.jl
+++ src/AtomicAndPhysicalConstants.jl
@@ -5,14 +5,14 @@ using Dates
 using HTTP
 using JSON
 
-include("physical_constants.jl");
-include("atomic_isotopes.jl");
-include("subatomic_species.jl");
-include("species_initialize.jl");
-include("update_pion_mass.jl");
-include("update_constants.jl");
-include("update_iso_masses.jl");
-include("particle_functions.jl");
-include("SetUnits.jl");
+include("physical_constants.jl")
+include("atomic_isotopes.jl")
+include("subatomic_species.jl")
+include("species_initialize.jl")
+include("update_pion_mass.jl")
+include("update_constants.jl")
+include("update_iso_masses.jl")
+include("particle_functions.jl")
+include("set_units.jl")
 
 end
