eb119f5166c3e0f51d17038a08b0fc2d2486b9d4
lixing <lilixing125@gmail.com>
2024-10-18 10:50:43 -0400

    make setunits return mass, create basis outline of setunit page

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,15 @@ using Documenter, AtomicAndPhysicalConstants
 
 makedocs(
   sitename="AtomicAndPhysicalConstants.jl",
-  authors = "Alex Coxe et al.",
-  format=Documenter.HTMLWriter.HTML(size_threshold = nothing),
-  pages = 
+  authors="Alex Coxe et al.",
+  format=Documenter.HTMLWriter.HTML(size_threshold=nothing),
+  pages=
   [
     "Home" => "index.md",
+    "Set Units" => "set_units.md"
   ]
 )
 
 deploydocs(;
-    repo = "github.com/bmad-sim/AtomicAndPhysicalConstants.jl.git",
+  repo="github.com/bmad-sim/AtomicAndPhysicalConstants.jl.git",
 )
---
+++ docs/src/set_units.md
@@ -0,0 +1,21 @@
+# setunits()
+## Syntax
+```julia
+setunits(unitsystem;
+  mass_unit,
+  length_unit,
+  time_unit,
+  energy_unit,
+  charge_unit,
+  print_units
+)
+```
+
+## Description
+
+
+### Options
+
+## Examples
+```julia
+```
