60a891e8d9507c81adf5f5d4f5b485c4f9cf3ef6
Lixing Li <lilixing125@gmail.com>
2025-01-18 15:01:38 +1030

    added structure to wrap constants

--- src/APCdef.jl
+++ src/APCdef.jl
@@ -76,20 +76,31 @@ It defines the physical constants and getter functions for species mass and char
 - `charge`: elementary charge
 
 """
-macro APCdef(exs...)
+macro APCdef(name=:APC, exs...)
+  #sort and collect the dictionary
+  pairs_sorted = sort(collect(CONSTANTS), by=x -> first(x))
+  # generate the field of the structure
+  fields = [:($key::Union{Quantity,Float64}) for (key, _) in pairs_sorted]
+
+  # define the structure with the field name as the constants name
+  struct_name = :ConstantsStructure
+  @eval struct $struct_name # fields are of type Float64
+    $(fields...)
+  end
+
   return quote
     #default parameter
-    local CODATA = 2022
-    local unitsystem = $ACCELERATOR
-    local unitful = false
-
+    CODATA = 2022
+    unitsystem = $ACCELERATOR
+    unitful = false
     #evaluate the parameter
     $(exs...)
-    local mass_unit = unitsystem[1]
-    local length_unit = unitsystem[2]
-    local time_unit = unitsystem[3]
-    local energy_unit = unitsystem[4]
-    local charge_unit = unitsystem[5]
+
+    mass_unit = unitsystem[1]
+    length_unit = unitsystem[2]
+    time_unit = unitsystem[3]
+    energy_unit = unitsystem[4]
+    charge_unit = unitsystem[5]
     # check dimensions of units
     if dimension(mass_unit) != dimension(u"kg")
       error("unit for mass does not have proper dimension")
@@ -106,28 +117,53 @@ macro APCdef(exs...)
     if dimension(charge_unit) != dimension(u"C")
       error("unit for charge does not have proper dimension")
     end
-    const $(esc(:C_LIGHT)) = unitful ? uconvert(length_unit / time_unit, $__b_c_light) : uconvert(length_unit / time_unit, $__b_c_light).val
-    const $(esc(:H_PLANCK)) = unitful ? uconvert(energy_unit * time_unit, $__b_h_planck) : uconvert(energy_unit * time_unit, $__b_h_planck).val #=  =#
-    const $(esc(:H_BAR_PLANCK)) = unitful ? uconvert(energy_unit * time_unit, $__b_h_bar_planck) : uconvert(energy_unit * time_unit, $__b_h_bar_planck).val
-    const $(esc(:R_E)) = unitful ? uconvert(length_unit, $__b_r_e) : uconvert(length_unit, $__b_r_e).val
-    const $(esc(:R_P)) = unitful ? uconvert(length_unit, $__b_r_p) : uconvert(length_unit, $__b_r_p).val
-    const $(esc(:E_CHARGE)) = unitful ? uconvert(charge_unit, $__b_e_charge) : uconvert(charge_unit, $__b_e_charge).val
-    const $(esc(:MU_0_VAC)) = unitful ? $__b_mu_0_vac : $__b_mu_0_vac.val
-    const $(esc(:EPS_0_VAC)) = unitful ? $__b_eps_0_vac : $__b_eps_0_vac.val
-    const $(esc(:CLASSICAL_RADIUS_FACTOR)) = unitful ? $__b_classical_radius_factor : $__b_classical_radius_factor.val
-    const $(esc(:FINE_STRUCTURE)) = $__b_fine_structure
-    const $(esc(:N_AVOGADRO)) = $__b_N_avogadro
 
-    const $(esc(:massof)) = (species::Species) -> unitful ? uconvert(mass_unit, species.mass) : uconvert(mass_unit, species.mass).val
-    const $(esc(:chargeof)) = (species::Species) -> unitful ? uconvert(charge_unit, species.charge) : uconvert(charge_unit, species.charge).val
-    const $(esc(:chargeof2)) = begin
-      (species::Species) -> if species.populated == IsDef.Full
-        (unitful ? uconvert(charge_unit, species.charge) : uconvert(charge_unit, species.charge).val)
-        # else
-        # 	error("""Can't get the charge of an undefined particle; 
-        # 	the Species constructor for this object was called with no arguments.""")
+    # how units of a certain dimension gets converted
+    conversion = Dict(
+      dimension(u"m/s") => length_unit / time_unit,
+      dimension(u"J*s") => energy_unit * time_unit,
+      dimension(u"m") => length_unit,
+      dimension(u"C") => charge_unit,
+    )
+
+
+    # generate the constructor parameter of the structure, converted to new units
+    values = [
+      if haskey(conversion, dimension(v))
+        if unitful
+          uconvert(conversion[dimension(v)], v)
+        else
+          uconvert(conversion[dimension(v)], v).val
+        end
+      else
+        if unitful
+          v
+        elseif v isa Float64
+          v
+        else
+          v.val
+        end
       end
+      for (_, v) in $pairs_sorted
+    ]
+
+    $(esc(name))::$ConstantsStructure = $ConstantsStructure(values...)
+
+    #massof and charge of
+    function $(esc(:massof))(species::Species)::Union{Float64,Quantity,Int64}
+      return unitful ? uconvert(mass_unit, species.mass) : uconvert(mass_unit, species.mass).val
     end
+    function $(esc(:chargeof))(species::Species)::Union{Float64,Quantity,Int64}
+      return unitful ? uconvert(charge_unit, species.charge) : uconvert(charge_unit, species.charge).val
+    end
+    #$(esc(:chargeof2)) = begin
+    #(species::Species) -> if species.populated == IsDef.Full
+    #(unitful ? uconvert(charge_unit, species.charge) : uconvert(charge_unit, species.charge).val)
+    # else
+    # 	error("""Can't get the charge of an undefined particle; 
+    # 	the Species constructor for this object was called with no arguments.""")
+    #end
+    #end
   end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -12,17 +12,17 @@ end
 
 @testset "testAPCdef" begin
   #constants should be of type float in the right unit
-  @test C_LIGHT ≈ 2.99792458e8
-  @test H_PLANCK ≈ 4.135667696e-15
-  @test H_BAR_PLANCK ≈ 6.582119568038699e-16
-  @test R_E ≈ 2.8179403205e-15
-  @test R_P ≈ 1.5346982640795807e-18
-  @test E_CHARGE ≈ 1
-  @test MU_0_VAC ≈ 1.25663706127e-6
-  @test EPS_0_VAC ≈ 8.8541878188e-12
-  @test CLASSICAL_RADIUS_FACTOR ≈ 1.4399645468825422e-15
-  @test FINE_STRUCTURE ≈ 0.0072973525643
-  @test N_AVOGADRO ≈ 6.02214076e23
+  @test APC.C_LIGHT ≈ 2.99792458e8
+  @test APC.H_PLANCK ≈ 4.135667696e-15
+  @test APC.H_BAR_PLANCK ≈ 6.582119568038699e-16
+  @test APC.R_E ≈ 2.8179403205e-15
+  @test APC.R_P ≈ 1.5346982640795807e-18
+  @test APC.E_CHARGE ≈ 1
+  @test APC.MU_0_VAC ≈ 1.25663706127e-6
+  @test APC.EPS_0_VAC ≈ 8.8541878188e-12
+  @test APC.CLASSICAL_RADIUS_FACTOR ≈ 1.4399645468825422e-15
+  @test APC.FINE_STRUCTURE ≈ 0.0072973525643
+  @test APC.N_AVOGADRO ≈ 6.02214076e23
 
   #test massof() and chargeof()
   H = Species("H")
