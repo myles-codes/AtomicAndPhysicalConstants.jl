1c0baf713709de6a05bf39b5f497e143e5185c46
Alexander Coxe <coxea3@EGR-KMQWXQ3Y4C.local>
2025-02-03 13:19:05 -0500

    fixed the downloadCODATA function so it doesn't run into negative numbers with 'findmin '

--- src/update_constants.jl
+++ src/update_constants.jl
@@ -50,7 +50,8 @@ downloadCODATA
 function downloadCODATA(year::Int)
     myYear = year
     NIST_releases = [1969, 1973, 1986, 1998, 2002, 2006, 2010, 2014, 2018, 2022]
-    ixyear = findmin(myYear .- NIST_releases)
+    diffs = abs.(myYear .- NIST_releases)
+    ixyear = findmin(diffs)
     year = NIST_releases[ixyear[2]]
     if ixyear[1] != 0
         println(f"The year requested isn't available: downloading the {year} CODATA release instead.")
