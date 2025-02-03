367aaeaa70be900354e01107f197501279e3ad77
lixing <lilixing125@gmail.com>
2025-02-03 16:47:43 -0500

    added documentations

--- src/APCdef.jl
+++ src/APCdef.jl
@@ -77,15 +77,21 @@ It defines the physical constants and getter functions for species mass and char
 
 """
 macro APCdef(name=:APC, exs...)
-  #sort and collect the dictionary
+  # sort and collect the dictionary
+  # since elements in the dictionary does not have a preferred order, sorting it increases stability
+  # pairs_sorted is an array of key and value pair of the CONSTANTS dictionary
   pairs_sorted = sort(collect(CONSTANTS), by=x -> first(x))
   # generate the field of the structure
+  # the name of the field is the key of the dictionary, i.e. symbols with capitalized names
+  # the type of the field is "Union{Quantity,Float64}", meaning that it could either be an Unitful Quantity or a Float, depending
+  # on user's choice
+  # fields is an array with elements are expressions like "C_LIGHT::Union{Quantity,Float64}"
   fields = [:($key::Union{Quantity,Float64}) for (key, _) in pairs_sorted]
 
   # define the structure with the field name as the constants name
-  struct_name = :ConstantsStructure
-  @eval struct $struct_name # fields are of type Float64
-    $(fields...)
+  struct_name = :ConstantsStructure # the name of the structure is ConstantsStructure
+  @eval struct $struct_name
+    $(fields...) # put the fields into the structure
   end
 
   return quote
@@ -96,6 +102,7 @@ macro APCdef(name=:APC, exs...)
     #evaluate the parameter
     $(exs...)
 
+    # extract the units from the unit system
     mass_unit = unitsystem[1]
     length_unit = unitsystem[2]
     time_unit = unitsystem[3]
@@ -118,10 +125,16 @@ macro APCdef(name=:APC, exs...)
       error("unit for charge does not have proper dimension")
     end
 
-    # how units of a certain dimension gets converted
+    # this dictionary maps the dimension of the unit to the target unit that it should convert to
     conversion = Dict(
+      dimension(u"kg") => mass_unit,
+      dimension(u"m") => length_unit,
+      dimension(u"s") => time_unit,
+      dimension(u"J") => energy_unit,
+      dimension(u"C") => charge_unit,
       dimension(u"m/s") => length_unit / time_unit,
       dimension(u"J*s") => energy_unit * time_unit,
+      dimension(u"kg*m") => mass_unit * length_unit,
       dimension(u"m") => length_unit,
       dimension(u"C") => charge_unit,
     )
@@ -129,24 +142,25 @@ macro APCdef(name=:APC, exs...)
 
     # generate the constructor parameter of the structure, converted to new units
     values = [
-      if haskey(conversion, dimension(v))
+      if haskey(conversion, dimension(v)) #if the dimension is one of the dimensions in the dictionary
         if unitful
-          uconvert(conversion[dimension(v)], v)
+          uconvert(conversion[dimension(v)], v) # if the user demands unitful quantity, return unitful quantity
         else
-          uconvert(conversion[dimension(v)], v).val
+          uconvert(conversion[dimension(v)], v).val # if the user demands Float, convert and return Float
         end
       else
-        if unitful
+        if unitful # whether user wants a unitful quantity 
+          v # if the unit dimension is not in the dictionary such as that of eps_0_vac, then just return the unit without any conversion
+        elseif v isa Float64 # If the value does not have unit, such as Avogadro's number
           v
-        elseif v isa Float64
-          v
-        else
+        else #If the user demands values in Float64
           v.val
         end
       end
-      for (_, v) in $pairs_sorted
+      for (_, v) in $pairs_sorted # for all the values in the dictionary
     ]
 
+    # initialize the structure with the constructor and values
     $(esc(name))::$ConstantsStructure = $ConstantsStructure(values...)
 
     #massof and charge of
