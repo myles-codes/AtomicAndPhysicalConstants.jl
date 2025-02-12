8577cbea036b5a11195b21071fd8db100c617037
lllx125 <lilixing125@gmail.com>
2025-02-11 20:30:12 -0500

    added access method

--- src/showconst.jl
+++ src/showconst.jl
@@ -1,10 +1,39 @@
+"""
+    showconst(
+        query::Symbol = :constants
+    )
 
-function showconst(query::Symbol)
+## Description:
+list available constants in the package
+
+## parameters:
+- `query`     -- type:`Symbol`, the type of constants you want to know about
+
+## options:
+- `:constants`      -- list all physical constants
+- `:subatomic`      -- list all possible subatomic particles
+- `:(atomic symbol)`-- list all isotopes of that atomic species, e.g. :Fe
+
+"""
+showconst
+
+function showconst(query::Symbol=:constants)
     # list all the physical constants and their values
-    if (query == :constants || query == :physical)
+    if (query == :constants)
         for (key, dict) in CODATA_Consts
-            for (_, value) in dict
-                println("- $key : $value")
+            for (name, value) in dict
+                println("- $key")
+                println("\t- base value: $value")
+                if (occursin("_m_", name)) # if the variable is the mass of a particle
+                    speciesname = split(name, "_m_")[2]
+                    println("\t- access by: massof(\"$speciesname\")")
+                elseif (occursin("_mu_", name)) # if the variable is the magnetic moment of a particle
+                    speciesname = split(name, "_mu_")[2]
+                    println("\t- access by: Species(\"$speciesname\").moment")
+                else #a physical constant
+                    constantname = uppercase(name[5:end])
+                    println("\t- access by: @APCdef; APC.$constantname")
+                end
             end
         end
         return
@@ -13,13 +42,7 @@ function showconst(query::Symbol)
     if (query == :subatomic)
         for (key, _) in SUBATOMIC_SPECIES
             println("- $key")
-        end
-        return
-    end
-    # list all the atomic species
-    if (query == :atomic)
-        for (key, _) in ATOMIC_SPECIES
-            println("- $key")
+            println("\t- access by: Species(\"$key\")")
         end
         return
     end
@@ -31,8 +54,10 @@ function showconst(query::Symbol)
         for (key, value) in sorted_dict
             if key == -1
                 println("- Average mass: $value")
+                println("\t- access by: Species(\"$query\")")
             else
                 println("- $query-$key : $value")
+                println("\t- access by: Species(\"$query#$key\")")
             end
         end
     else
