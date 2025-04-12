267a324ed092d816ba5c00abf5ad8f37455e9fc5
lixing <lilixing125@gmail.com>
2025-04-12 16:41:51 -0400

    fixed

--- src/AtomicAndPhysicalConstants.jl
+++ src/AtomicAndPhysicalConstants.jl
@@ -1,33 +1,7 @@
 module AtomicAndPhysicalConstants
 
-
-export @APCdef
-export ACCELERATOR, MKS, CGS
-export Species
-export SubatomicSpecies
-export AtomicSpecies
-export SUBATOMIC_SPECIES
-export ATOMIC_SPECIES
-export useCODATA
-export NewUnits
-export showconst
-export full_name, atomicnumber, g_spin, gyromagnetic_anomaly, g_nucleon
-export Kind
-export ATOM, HADRON, LEPTON, PHOTON, NULL
-export setisos
 export CODATA_releases
 export CODATA2002, CODATA2006, CODATA2010, CODATA2014, CODATA2018, CODATA2022
-
-
-using PyFormattedStrings
-using Dates
-using HTTP
-using JSON
-using EnumX
-using Reexport
-using AtomicAndPhysicalConstants.NewUnits
-@reexport using Unitful
-import DynamicQuantities
 
 
 include("units_definition.jl")
