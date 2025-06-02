932a23661b5c96e00a2d301b19e89f96b9c5416b
Alexander Coxe <acoxe.dev@gmail.com>
2025-06-02 15:14:49 -0400

    removed full_name() docstring

--- src/functions.jl
+++ src/functions.jl
@@ -100,15 +100,6 @@ end;
 
 
 
-"""
-		full_name(species::Species)
-
-get the full name of a tracked species:
-- if species is subatomic, gives the name in the SUBATOMIC_SPECIES dictionary
-- if species is atomic, gives "mass number" * "atomic symbol" * "charge state", 
-  *e.g.* #3He-1 for a Helion with 3 bound electrons
-"""
-full_name
 
 
 
